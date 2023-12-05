c0a3190c6505c25a783c4f2cf5560943ceecc797
mikailyan <103881811+mikailyan@users.noreply.github.com>
2023-12-05 20:22:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
 import {Route, Routes} from 'react-router-dom'
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 
 function App() {
-    const {onToggleButton, tg} = useTelegram();
 
     useEffect(() => {
         tg.ready();
